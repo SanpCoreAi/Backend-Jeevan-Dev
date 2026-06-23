94c1f28eadc19a92739b51a90bb9455b85863afd
subhaskushwaha <subhaskushwahak@gmail.com>
2026-06-23 18:53:51 +0530

    register api update

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -32,7 +32,7 @@ router.get( "/getUsers", verifyToken, getUsers);
 
 router.get( "/getUserByDoctorId/:doctorId", verifyToken, getUserByDoctorId);
 
-router.get( "/getUserByDoctorAssistant", verifyToken, getUserByDoctorAssistant);
+router.get( "/getUserByDoctorAssistant",allowRoles(2), verifyToken, getUserByDoctorAssistant);
 
 router.get(
   "/assistant/stats",
